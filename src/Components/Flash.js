2967f6c1e030a02c7e2f1d514164147aca7b1ff9
Esha Patel <Patellesha11@gmail.com>
2025-04-30 19:16:54 +0300

    added id 3

--- src/Components/Flash.js
+++ src/Components/Flash.js
@@ -4,4 +4,5 @@ import "./Flash.css";
 const flashcards = [
     { id: 1, question: 'What is the capital of France?', answer: 'Paris' },
     { id: 2, question: 'Who wrote Hamlet?', answer: 'William Shakespeare' },
+    { id: 3, question: 'What is the speed of light?', answer: '299,792,458 m/s' }
 ]
