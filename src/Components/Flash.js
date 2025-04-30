eb1029abd5a0414b695881c65e2d3e41cba81c53
Esha Patel <Patellesha11@gmail.com>
2025-04-30 19:16:40 +0300

    added id 2

--- src/Components/Flash.js
+++ src/Components/Flash.js
@@ -3,4 +3,5 @@ import "./Flash.css";
 
 const flashcards = [
     { id: 1, question: 'What is the capital of France?', answer: 'Paris' },
+    { id: 2, question: 'Who wrote Hamlet?', answer: 'William Shakespeare' },
 ]
